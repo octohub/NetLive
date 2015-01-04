83bcf3b9bf1cec28db09399c8229371445006e7f
Richard Lucas <richard.lucas@raizlabs.com>
2015-01-03 16:47:05 -0800

    Nice

--- NetLive/src/main/java/com/richardlucasapps/netlive/MainActivity.java
+++ NetLive/src/main/java/com/richardlucasapps/netlive/MainActivity.java
@@ -204,7 +204,7 @@ public class MainActivity extends Activity {
 	private void showAboutDialog() {
 		 AlertDialog.Builder aboutBuilder = new AlertDialog.Builder(this);
 		 TextView myMsg = new TextView(this);
-		 SpannableString s = new SpannableString("NetLive v3.0 Beta\n\nrichardlucasapps.com");
+		 SpannableString s = new SpannableString("NetLive v3.1 Beta\n\nrichardlucasapps.com");
 		 Linkify.addLinks(s, Linkify.WEB_URLS);
 		 myMsg.setText(s);
 		 myMsg.setTextSize(15);
